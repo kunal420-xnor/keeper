c3c357d82e9619021eaec87e175032f431756b0d
kunal420-xnor <83002916+kunal420-xnor@users.noreply.github.com>
2023-07-05 01:07:03 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1 contenEditable="true"> Title </h1>
+      <p contentEditable="true"> content </p>
+    </div>
+  );
+}
+
+export default Note;
